602255b4e844bafe87871f6b893038a51ed0d15d
Kushul Soomaree <kushul@lsl.digital>
2020-06-11 22:32:52 +0400

    header and footer fix

---
+++ frontend/src/components/shared/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-footer
+    padless
+    dark
+    fix
+    class="v-footer blue-grey font-weight-medium darken-4 elevation-2 v-sheet v-sheet--tile"
+    data-booted="true"
+  >
+    <div class="text-center d-flex justify-center align-center col col-12">
+      2020 —
+      <strong class="ml-1">Surveys</strong>
+      <v-btn-toggle
+        flat
+        icon
+        small
+        class="transparent elevation-0 ml-2"
+        v-model="$vuetify.theme.dark"
+      >
+        <v-btn flat icon small :value="true" class="transparent">
+          <v-icon>invert_colors</v-icon>
+        </v-btn>
+      </v-btn-toggle>
+    </div>
+  </v-footer>
+</template>
---
+++ frontend/src/components/shared/NoDataAlert.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-alert
+    outlined
+    :value="true"
+    class="alert-listing-wrapper ma-0 d-flex justify-center"
+    dark
+    color="info"
+    icon="info"
+  >
+    <span>{{ alertMessage }}</span>
+  </v-alert>
+</template>
+
+<script>
+export default {
+  props: {
+    alertMessage: {
+      type: String
+    }
+  }
+};
+</script>
+
+<style>
+.alert-listing-wrapper div {
+  flex: unset !important;
+}
+</style>
