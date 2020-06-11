b1e3c8afd85bc6cb1c88e4915973467d00143d86
Kushul Soomaree <kushul@lsl.digital>
2020-06-12 00:12:22 +0400

    favicon

--- frontend/src/components/shared/Footer.vue
+++ frontend/src/components/shared/Footer.vue
@@ -9,14 +9,8 @@
     <div class="text-center d-flex justify-center align-center col col-12">
       2020 —
       <strong class="ml-1">Surveys</strong>
-      <v-btn-toggle
-        flat
-        icon
-        small
-        class="transparent elevation-0 ml-2"
-        v-model="$vuetify.theme.dark"
-      >
-        <v-btn flat icon small :value="true" class="transparent">
+      <v-btn-toggle icon small class="transparent elevation-0 ml-2" v-model="$vuetify.theme.dark">
+        <v-btn icon small :value="true" class="transparent">
           <v-icon>invert_colors</v-icon>
         </v-btn>
       </v-btn-toggle>
