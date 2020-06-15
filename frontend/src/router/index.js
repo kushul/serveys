19b01ccc0155192ee83fe9c3c4bdca176f79a834
Kushul Soomaree <kushul@lsl.digital>
2020-06-15 17:18:18 +0400

    remove about page

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -18,14 +18,6 @@ const routes = [
     component: ServeyFullView
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
     path: "*",
     name: "not-found",
     component: Page404,
